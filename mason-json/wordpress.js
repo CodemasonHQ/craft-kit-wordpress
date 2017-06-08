ceaf38e95251229d0ccce751d2c4299e210b1ab7
Ben M <benmag@users.noreply.github.com>
2017-06-08 22:24:03 +1000

    Replace old instance definition with `build` and `base` values

--- mason-json/wordpress.js
+++ mason-json/wordpress.js
@@ -1,12 +1,14 @@
 module.exports = {
-    name: 'wordpress', 
-    image: 'wordpress',
-    type: 'instance',
+    name: 'app', 
+    build: '.',
+    base: 'wordpress', 
+    type: 'service',
     volumes: {
         './':'/var/www/html',
     },
     ports: [
         "80:80",
+        "443:443"
     ],
     environment: {
       'WORDPRESS_DB_PASSWORD': 'example',
