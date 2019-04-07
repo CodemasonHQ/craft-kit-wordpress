f73bfc045f24edc244af3252985bd9caf4ab7842
benmag <benmag@users.noreply.github.com>
2019-04-07 23:38:50 +1000

    Fix indentation

--- mason-json/wordpress.js
+++ mason-json/wordpress.js
@@ -1,16 +1,16 @@
 module.exports = {
-    name: 'app', 
-    build: '.',
-    base: 'wordpress', 
-    type: 'service',
-    volumes: {
-        './':'/var/www/html',
-    },
-    ports: [
-        "80:80",
-        "443:443"
-    ],
-    environment: {
-      'WORDPRESS_DB_PASSWORD': 'example',
-    }
+  name: 'web', 
+  build: '.',
+  base: 'wordpress', 
+  type: 'service',
+  volumes: {
+    './':'/var/www/html',
+  },
+  ports: [
+    "80:80",
+    "443:443"
+  ],
+  environment: {
+    'WORDPRESS_DB_PASSWORD': 'example',
+  }
 }
